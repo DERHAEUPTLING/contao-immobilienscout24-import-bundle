566299e1a268b9cd03d229abbf119d753f09a8ec
M. Vondano <m-vo@users.noreply.github.com>
2019-10-03 16:32:16 +0200

    cleanup config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,17 +13,4 @@ declare(strict_types=1);
 // backend
 $GLOBALS['BE_MOD']['system']['immoscout24_accounts'] = [
     'tables' => ['tl_immoscout24_account'],
-    //'synchronizePosts' => ['mvo_contao_facebook.datacontainer.facebook_node', 'onSynchronizePosts'],
-    //'synchronizeEvents' => ['mvo_contao_facebook.datacontainer.facebook_node', 'onSynchronizeEvents'],
 ];
-//
-//if ('BE' === TL_MODE) {
-//    $GLOBALS['TL_CSS'][] = 'bundles/mvocontaofacebookimport/css/backend.css';
-//}
-//
-//// content elements
-//$GLOBALS['TL_CTE']['mvo_facebook']['mvo_facebook_post_list'] = ContentPostList::class;
-//$GLOBALS['TL_CTE']['mvo_facebook']['mvo_facebook_event_list'] = ContentEventList::class;
-//
-//// background synchronization
-//$GLOBALS['TL_CRON']['minutely'][] = ['mvo_contao_facebook.listener.contao_cron_listener', 'onExecuteByContaoCron'];
