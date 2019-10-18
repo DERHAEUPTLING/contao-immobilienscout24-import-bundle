d2bcb03b3d37c5df27c14d63114cf1b5f4ae51b6
M. Vondano <m-vo@users.noreply.github.com>
2019-10-18 13:23:46 +0200

    add filtering fields

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -10,15 +10,38 @@ declare(strict_types=1);
  * @license    MIT
  */
 
-use Derhaeuptling\ContaoImmoscout24\EventListener\Account;
-
-$GLOBALS['TL_DCA']['tl_module']['palettes']['immoscout24_real_estate_list'] = '{title_legend},name,headline,type;{immoscout24_legend},immoscout24_accounts;{redirect_legend},jumpTo;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['immoscout24_real_estate_list'] = '{title_legend},name,headline,type;{immoscout24_legend},immoscout24_accounts;immoscout24_filter_explanation,immoscout24_filter;immoscout24_number_of_items;{redirect_legend},jumpTo;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['immoscout24_real_estate_reader'] = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['immoscout24_accounts'] = [
     'exclude' => true,
     'eval' => ['multiple' => true, 'tl_class' => 'w50'],
     'inputType' => 'checkboxWizard',
-    'options_callback' => [Account::class, 'listAccounts'],
     'sql' => 'blob NULL',
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['immoscout24_filter_explanation'] = [
+    'exclude' => true,
+    'eval' => ['tl_class' => 'w100 clr'],
+    'input_field_callback' => static function (Contao\DataContainer $dc) {
+        return sprintf(
+            '<div class="widget"><p class="tl_info">%s</p></div>',
+            $GLOBALS['TL_LANG']['tl_module']['immoscout24_filter_explanation']
+        );
+    },
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['immoscout24_filter'] = [
+    'exclude' => true,
+    'eval' => ['tl_class' => 'w50 clr'],
+    'inputType' => 'text',
+    'sql' => "varchar(256) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['immoscout24_number_of_items'] = [
+    'exclude' => true,
+    'default' => 0,
+    'eval' => ['mandatory' => true, 'rgxp' => 'natural', 'tl_class' => 'w50 clr'],
+    'inputType' => 'text',
+    'sql' => 'smallint(5) unsigned NOT NULL default 0',
 ];
--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -4,3 +4,10 @@ declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['tl_module']['immoscout24_legend'] = 'Immoscout24';
 $GLOBALS['TL_LANG']['tl_module']['immoscout24_accounts'] = ['Accounts'];
+$GLOBALS['TL_LANG']['tl_module']['immoscout24_filter'] = ['Filter expression', '// todo'];
+$GLOBALS['TL_LANG']['tl_module']['immoscout24_number_of_items'] = ['Number of items', 'Enter 0 to list all (no limiting).'];
+
+$GLOBALS['TL_LANG']['tl_module']['immoscout24_filter_explanation'] =
+    <<<'TAG'
+// todo
+TAG;
