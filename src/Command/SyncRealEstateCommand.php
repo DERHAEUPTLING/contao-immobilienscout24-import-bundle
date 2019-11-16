6295227328b0e16db6daa8b79b50fd3fae494b21
M. Vondano <m-vo@users.noreply.github.com>
2019-11-16 13:35:22 +0100

    add purge option to sync command

--- src/Command/SyncRealEstateCommand.php
+++ src/Command/SyncRealEstateCommand.php
@@ -15,6 +15,7 @@ namespace Derhaeuptling\ContaoImmoscout24\Command;
 use Derhaeuptling\ContaoImmoscout24\Entity\Account;
 use Derhaeuptling\ContaoImmoscout24\Repository\AccountRepository;
 use Derhaeuptling\ContaoImmoscout24\Synchronizer\SynchronizerFactory;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,12 +30,16 @@ class SyncRealEstateCommand extends Command
     /** @var AccountRepository */
     private $accountRepository;
 
-    public function __construct(SynchronizerFactory $synchronizerFactory, AccountRepository $accountRepository)
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(SynchronizerFactory $synchronizerFactory, AccountRepository $accountRepository, Connection $connection)
     {
         parent::__construct();
 
         $this->synchronizerFactory = $synchronizerFactory;
         $this->accountRepository = $accountRepository;
+        $this->connection = $connection;
     }
 
     protected function configure(): void
@@ -42,13 +47,30 @@ class SyncRealEstateCommand extends Command
         $this
             ->setDescription('Synchronizes the database with the API.')
             ->addArgument('id', InputArgument::OPTIONAL, 'Account id or description.')
+            ->addOption('purge', 'p', InputOption::VALUE_NONE, 'Purge the real estate table.')
             ->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Run dry, do not apply changes.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $purge = $input->getOption('purge');
         $dryRun = $input->getOption('dry-run');
+
+        if ($purge && $dryRun) {
+            $output->writeln('Cannot use option `purge` together with `dry-run`. Aborting.');
+
+            return 1;
+        }
+
+        if ($purge) {
+            // note: do not use truncate - we need to execute per-row actions (such as FK cascades)
+            /* @noinspection SqlWithoutWhere */
+            $this->connection->executeQuery('DELETE FROM tl_immoscout24_real_estate');
+
+            $output->writeln('Purged real estate table.');
+        }
+
         if ($dryRun) {
             $output->writeln('Dry running without applying changes...');
         }
