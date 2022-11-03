73908e86924b57ebb202351c754d121bf573f1ba
M. Vondano <m-vo@users.noreply.github.com>
2022-11-03 15:09:25 +0100

    remove dependency on haste

--- src/Resources/contao/dca/tl_immoscout24_account.php
+++ src/Resources/contao/dca/tl_immoscout24_account.php
@@ -33,31 +33,18 @@ $GLOBALS['TL_DCA']['tl_immoscout24_account'] =
             'global_operations' => [],
             'operations' => [
                 'edit' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_immoscout24_account']['edit'],
                     'href' => 'act=edit',
                     'icon' => 'edit.svg',
                 ],
-                'enable' => [
+                'toggle' => [
                     'label' => &$GLOBALS['TL_LANG']['tl_immoscout24_account']['enable'],
-                    'attributes' => 'onclick="Backend.getScrollOffset();"',
-                    'haste_ajax_operation' => [
-                        'field' => 'enabled',
-                        'options' => [
-                            [
-                                'value' => '0',
-                                'icon' => 'invisible.svg',
-                            ],
-                            [
-                                'value' => '1',
-                                'icon' => 'visible.svg',
-                            ],
-                        ],
-                    ],
+                    'href' => 'act=toggle&amp;field=enabled',
+                    'icon' => 'visible.svg',
                 ],
                 'delete' => [
-                    'label' => &$GLOBALS['TL_LANG']['tl_immoscout24_account']['delete'],
                     'href' => 'act=delete',
                     'icon' => 'delete.svg',
+                    'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '').'\'))return false;Backend.getScrollOffset()"',
                 ],
             ],
         ],
@@ -148,6 +135,7 @@ $GLOBALS['TL_DCA']['tl_immoscout24_account'] =
                 'exclude' => true,
                 'default' => 0,
                 'inputType' => 'checkbox',
+                'toggle' => true,
                 'eval' => ['isBoolean' => true],
                 'save_callback' => [
                     static function ($value) {
